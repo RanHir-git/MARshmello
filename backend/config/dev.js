d14dae9005072272293960d8326035bf5f86a716
Ran Hirschorn <ranhirschorn@gmail.com>
2026-01-07 12:37:48 +0200

    safeguard against XSS in dev config

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,6 +1,5 @@
 export default {
     dbURL: 'mongodb://127.0.0.1:27017',
-    // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
     dbName: 'marshmelloDB',
     cloudinary: {
         cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
